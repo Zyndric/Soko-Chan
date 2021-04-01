4118db1901f27dc4de45559bdf49e6935ed5885e
Zyndric <alexander@roehnsch.de>
2021-04-01 10:47:03 +0200

    Refactored level initialization

--- main.ts
+++ main.ts
@@ -12,6 +12,10 @@ namespace SpriteKind {
  */
 /**
  * Set up
+ * 
+ * Variables ban, "undo ban" and "undo box" are unique and used by name.
+ * 
+ * Variables box, c and t are loop and temporary variables.
  */
 /**
  * Determine if a box is on a specific tile by comparing their absolute x and y pixel coordiates. Use pixels, because the color-coded Tile object lacks a mechanism to get its tileset coordinates.
@@ -22,30 +26,15 @@ controller.down.onEvent(ControllerButtonEvent.Released, function () {
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
     pressed_up = 0
 })
-/**
- * Tile coding:
- * 
- * 14 brown  -- wall (#)
- * 
- *   3 pink     -- target (.)
- * 
- *   7 green   -- player (@)
- * 
- *   6 teal      -- player on target (+)
- * 
- *   4 orange -- crate ($)
- * 
- *   2 red       -- crate on target (*)
- * 
- * 13 tan       -- floor
- */
 function init_states () {
     pressed_up = 0
     pressed_down = 0
     pressed_left = 0
     pressed_right = 0
+    pressed_B = 0
     undo_ban = []
     undo_box = []
+    info.setScore(0)
 }
 function sprite_cache () {
     box = sprites.create(img`
@@ -711,6 +700,20 @@ function show_menu () {
         }
     }
 }
+function prepare_level () {
+    init_states()
+    scene.setTileMap(img`
+        . . e e e e . . . . 
+        . . e d 3 e . . . . 
+        . . e d d e e e . . 
+        . . e 2 7 d d e . . 
+        . . e d d 4 d e . . 
+        . . e d d e e e . . 
+        . . e e e e . . . . 
+        `)
+    scene.cameraFollowSprite(ban)
+    realize_tilemap()
+}
 function move_to (tx: number, ty: number, push_tx: number, push_ty: number) {
     if (!(tiles.tileIsWall(tiles.getTileLocation(tx, ty)))) {
         if (box_on_tile(tx, ty)) {
@@ -741,6 +744,10 @@ function walk (dtx: number, dty: number) {
 controller.left.onEvent(ControllerButtonEvent.Released, function () {
     pressed_left = 0
 })
+function next_level () {
+    level += 1
+    prepare_level()
+}
 function all_boxes_fit () {
     for (let c of sprites.allOfKind(SpriteKind.Crate)) {
         if (!(target_tile(c.x, c.y))) {
@@ -749,16 +756,140 @@ function all_boxes_fit () {
     }
     return 1
 }
-function init_level_by_tilemap () {
-    scene.setTileMap(img`
-        . . e e e e . . . . 
-        . . e d 3 e . . . . 
-        . . e d d e e e . . 
-        . . e 2 7 d d e . . 
-        . . e d d 4 d e . . 
-        . . e d d e e e . . 
-        . . e e e e . . . . 
-        `)
+function undo_move () {
+    if (undo_ban.length == 2) {
+        move_ban(undo_ban[0], undo_ban[1])
+        info.changeScoreBy(1)
+    }
+    if (undo_box.length == 4) {
+        move_box(undo_box[2], undo_box[3], undo_box[0], undo_box[1])
+    }
+    undo_ban = []
+    undo_box = []
+}
+controller.B.onEvent(ControllerButtonEvent.Released, function () {
+    pressed_B = 0
+})
+function move_ban (to_tx: number, to_ty: number) {
+    undo_ban = [tiles.locationXY(tiles.locationOfSprite(ban), tiles.XY.column), tiles.locationXY(tiles.locationOfSprite(ban), tiles.XY.row)]
+    tiles.placeOnTile(ban, tiles.getTileLocation(to_tx, to_ty))
+    if (target_tile(tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.x), tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.y))) {
+        ban.setImage(img`
+            . . f f f . f f f f . f f f . . 
+            . f f f f f c c c c f f f f f . 
+            . f f f f b c c c c b f f f f . 
+            . f f f b a c c c c a b f f f . 
+            . . f a a c c c c c c a a f . . 
+            . . f c c c c 4 4 c c c c f . . 
+            . . f f c c 4 4 4 4 c c f f . . 
+            . . f f f b f 4 4 f b f f f . . 
+            . . f f 4 1 f d d f 1 4 f f . . 
+            . . . f f d d d d d d f f . . . 
+            . . . e f e 4 4 4 4 e f e . . . 
+            . . e 4 f b a a a a b f 4 e . . 
+            . . 4 d f a a a a a a c d 4 . . 
+            . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
+    } else {
+        ban.setImage(img`
+            . . f f f . f f f f . f f f . . 
+            . f f f f f c c c c f f f f f . 
+            . f f f f b c c c c b f f f f . 
+            . f f f c 3 c c c c 3 c f f f . 
+            . . f 3 3 c c c c c c 3 3 f . . 
+            . . f c c c c 4 4 c c c c f . . 
+            . . f f c c 4 4 4 4 c c f f . . 
+            . . f f f b f 4 4 f b f f f . . 
+            . . f f 4 1 f d d f 1 4 f f . . 
+            . . . f f d d d d d d f f . . . 
+            . . . e f e 4 4 4 4 e f e . . . 
+            . . e 4 f b 3 3 3 3 b f 4 e . . 
+            . . 4 d f 3 3 3 3 3 3 c d 4 . . 
+            . . 4 4 f 6 6 6 6 6 6 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
+    }
+}
+function box_on_tile (tx: number, ty: number) {
+    for (let c of sprites.allOfKind(SpriteKind.Crate)) {
+        if (tiles.locationXY(tiles.locationOfSprite(c), tiles.XY.column) == tx && tiles.locationXY(tiles.locationOfSprite(c), tiles.XY.row) == ty) {
+            return 1
+        }
+    }
+    return 0
+}
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    pressed_right = 0
+})
+function move_box (from_tx: number, from_ty: number, to_tx: number, to_ty: number) {
+    for (let c of sprites.allOfKind(SpriteKind.Crate)) {
+        if (c.x == tiles.locationXY(tiles.getTileLocation(from_tx, from_ty), tiles.XY.x) && c.y == tiles.locationXY(tiles.getTileLocation(from_tx, from_ty), tiles.XY.y)) {
+            undo_box = [from_tx, from_ty, to_tx, to_ty]
+            tiles.placeOnTile(c, tiles.getTileLocation(to_tx, to_ty))
+            if (target_tile(tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.x), tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.y))) {
+                c.setImage(img`
+                    . f f f f f f f f f f f f f f . 
+                    f e e e e e e e e e e e e e e f 
+                    f e e e e e e e e e e e e e e f 
+                    f f f f f f f f f f f f f f f f 
+                    f e e f 4 4 4 4 4 f e e f e e f 
+                    f e e f 4 4 4 4 f e e e f e e f 
+                    f e e f 4 4 4 f e e e f f e e f 
+                    f e e f 4 4 f e e e f 4 f e e f 
+                    f e e f 4 f e e e f 4 4 f e e f 
+                    f e e f f e e e f 4 4 4 f e e f 
+                    f e e f e e e f 4 4 4 4 f e e f 
+                    f e e f e e f 4 4 4 4 4 f e e f 
+                    f f f f f f f f f f f f f f f f 
+                    f e e e e e e e e e e e e e e f 
+                    f e e e e e e e e e e e e e e f 
+                    . f f f f f f f f f f f f f f . 
+                    `)
+            } else {
+                c.setImage(img`
+                    . f f f f f f f f f f f f f f . 
+                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+                    f e e e e e e e e e e e e e e f 
+                    f 4 4 e 4 4 4 4 4 e 4 4 e 4 4 f 
+                    f 4 4 e 4 4 4 4 e 4 4 4 e 4 4 f 
+                    f 4 4 e 4 4 4 e 4 4 4 e e 4 4 f 
+                    f 4 4 e 4 4 e 4 4 4 e 4 e 4 4 f 
+                    f 4 4 e 4 e 4 4 4 e 4 4 e 4 4 f 
+                    f 4 4 e e 4 4 4 e 4 4 4 e 4 4 f 
+                    f 4 4 e 4 4 4 e 4 4 4 4 e 4 4 f 
+                    f 4 4 e 4 4 e 4 4 4 4 4 e 4 4 f 
+                    f e e e e e e e e e e e e e e f 
+                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
+                    . f f f f f f f f f f f f f f . 
+                    `)
+            }
+            return
+        }
+    }
+}
+/**
+ * Tile coding:
+ * 
+ * 14 brown  -- wall (#)
+ * 
+ *   3 pink     -- target (.)
+ * 
+ *   7 green   -- player (@)
+ * 
+ *   6 teal      -- player on target (+)
+ * 
+ *   4 orange -- crate ($)
+ * 
+ *   2 red       -- crate on target (*)
+ * 
+ * 13 tan       -- floor
+ */
+function realize_tilemap () {
     for (let t of scene.getTilesByType(2)) {
         box = sprites.create(img`
             . f f f f f f f f f f f f f f . 
@@ -901,124 +1032,6 @@ function init_level_by_tilemap () {
         b b b c b b b b b b b b b b b b 
         c c c c c c c c c c c c c c c c 
         `, true)
-    scene.cameraFollowSprite(ban)
-    info.setScore(0)
-}
-function undo_move () {
-    if (undo_ban.length == 2) {
-        move_ban(undo_ban[0], undo_ban[1])
-        info.changeScoreBy(1)
-    }
-    if (undo_box.length == 4) {
-        move_box(undo_box[2], undo_box[3], undo_box[0], undo_box[1])
-    }
-    undo_ban = []
-    undo_box = []
-}
-controller.B.onEvent(ControllerButtonEvent.Released, function () {
-    pressed_B = 0
-})
-function move_ban (to_tx: number, to_ty: number) {
-    undo_ban = [tiles.locationXY(tiles.locationOfSprite(ban), tiles.XY.column), tiles.locationXY(tiles.locationOfSprite(ban), tiles.XY.row)]
-    tiles.placeOnTile(ban, tiles.getTileLocation(to_tx, to_ty))
-    if (target_tile(tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.x), tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.y))) {
-        ban.setImage(img`
-            . . f f f . f f f f . f f f . . 
-            . f f f f f c c c c f f f f f . 
-            . f f f f b c c c c b f f f f . 
-            . f f f b a c c c c a b f f f . 
-            . . f a a c c c c c c a a f . . 
-            . . f c c c c 4 4 c c c c f . . 
-            . . f f c c 4 4 4 4 c c f f . . 
-            . . f f f b f 4 4 f b f f f . . 
-            . . f f 4 1 f d d f 1 4 f f . . 
-            . . . f f d d d d d d f f . . . 
-            . . . e f e 4 4 4 4 e f e . . . 
-            . . e 4 f b a a a a b f 4 e . . 
-            . . 4 d f a a a a a a c d 4 . . 
-            . . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f . . f f . . . . . 
-            `)
-    } else {
-        ban.setImage(img`
-            . . f f f . f f f f . f f f . . 
-            . f f f f f c c c c f f f f f . 
-            . f f f f b c c c c b f f f f . 
-            . f f f c 3 c c c c 3 c f f f . 
-            . . f 3 3 c c c c c c 3 3 f . . 
-            . . f c c c c 4 4 c c c c f . . 
-            . . f f c c 4 4 4 4 c c f f . . 
-            . . f f f b f 4 4 f b f f f . . 
-            . . f f 4 1 f d d f 1 4 f f . . 
-            . . . f f d d d d d d f f . . . 
-            . . . e f e 4 4 4 4 e f e . . . 
-            . . e 4 f b 3 3 3 3 b f 4 e . . 
-            . . 4 d f 3 3 3 3 3 3 c d 4 . . 
-            . . 4 4 f 6 6 6 6 6 6 f 4 4 . . 
-            . . . . . f f f f f f . . . . . 
-            . . . . . f f . . f f . . . . . 
-            `)
-    }
-}
-function box_on_tile (tx: number, ty: number) {
-    for (let c of sprites.allOfKind(SpriteKind.Crate)) {
-        if (tiles.locationXY(tiles.locationOfSprite(c), tiles.XY.column) == tx && tiles.locationXY(tiles.locationOfSprite(c), tiles.XY.row) == ty) {
-            return 1
-        }
-    }
-    return 0
-}
-controller.right.onEvent(ControllerButtonEvent.Released, function () {
-    pressed_right = 0
-})
-function move_box (from_tx: number, from_ty: number, to_tx: number, to_ty: number) {
-    for (let c of sprites.allOfKind(SpriteKind.Crate)) {
-        if (c.x == tiles.locationXY(tiles.getTileLocation(from_tx, from_ty), tiles.XY.x) && c.y == tiles.locationXY(tiles.getTileLocation(from_tx, from_ty), tiles.XY.y)) {
-            undo_box = [from_tx, from_ty, to_tx, to_ty]
-            tiles.placeOnTile(c, tiles.getTileLocation(to_tx, to_ty))
-            if (target_tile(tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.x), tiles.locationXY(tiles.getTileLocation(to_tx, to_ty), tiles.XY.y))) {
-                c.setImage(img`
-                    . f f f f f f f f f f f f f f . 
-                    f e e e e e e e e e e e e e e f 
-                    f e e e e e e e e e e e e e e f 
-                    f f f f f f f f f f f f f f f f 
-                    f e e f 4 4 4 4 4 f e e f e e f 
-                    f e e f 4 4 4 4 f e e e f e e f 
-                    f e e f 4 4 4 f e e e f f e e f 
-                    f e e f 4 4 f e e e f 4 f e e f 
-                    f e e f 4 f e e e f 4 4 f e e f 
-                    f e e f f e e e f 4 4 4 f e e f 
-                    f e e f e e e f 4 4 4 4 f e e f 
-                    f e e f e e f 4 4 4 4 4 f e e f 
-                    f f f f f f f f f f f f f f f f 
-                    f e e e e e e e e e e e e e e f 
-                    f e e e e e e e e e e e e e e f 
-                    . f f f f f f f f f f f f f f . 
-                    `)
-            } else {
-                c.setImage(img`
-                    . f f f f f f f f f f f f f f . 
-                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-                    f e e e e e e e e e e e e e e f 
-                    f 4 4 e 4 4 4 4 4 e 4 4 e 4 4 f 
-                    f 4 4 e 4 4 4 4 e 4 4 4 e 4 4 f 
-                    f 4 4 e 4 4 4 e 4 4 4 e e 4 4 f 
-                    f 4 4 e 4 4 e 4 4 4 e 4 e 4 4 f 
-                    f 4 4 e 4 e 4 4 4 e 4 4 e 4 4 f 
-                    f 4 4 e e 4 4 4 e 4 4 4 e 4 4 f 
-                    f 4 4 e 4 4 4 e 4 4 4 4 e 4 4 f 
-                    f 4 4 e 4 4 e 4 4 4 4 4 e 4 4 f 
-                    f e e e e e e e e e e e e e e f 
-                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-                    f 4 4 4 4 4 4 4 4 4 4 4 4 4 4 f 
-                    . f f f f f f f f f f f f f f . 
-                    `)
-            }
-            return
-        }
-    }
 }
 function target_tile (x: number, y: number) {
     for (let t of scene.getTilesByType(3)) {
@@ -1030,17 +1043,17 @@ function target_tile (x: number, y: number) {
     }
     return 0
 }
-let pressed_B = 0
 let ban: Sprite = null
 let box: Sprite = null
 let undo_box: number[] = []
 let undo_ban: number[] = []
+let pressed_B = 0
 let pressed_right = 0
 let pressed_left = 0
 let pressed_up = 0
 let pressed_down = 0
-init_states()
-init_level_by_tilemap()
+let level = 0
+next_level()
 forever(function () {
     if (all_boxes_fit()) {
         game.over(true)
